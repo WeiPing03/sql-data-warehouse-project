a25245bbb7ce3bd185e9afd776236a3c2e515d12
WeiPing03 <weiping030212@gmail.com>
2026-08-16 11:49:13 +0800

    Create ddl_gold.sql

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,7 @@
+/*
+=============================================================================
+DDL Script: Create Gold Views
+=============================================================================
+Script Purpose:
+   This script creates views for the Gold layer in the data warehouse.
+   This Gold
